51fd77bdd6034d621d9338c3cdce1339d34c8866
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-11-13 23:31:14 +0330

    config _app.js and create _document.js files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title> Home | Countries </title>
+      </Head>
+      {getLayout(<Component {...pageProps} />, pageProps)}
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+
+class LocalizedDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    // locale is in ctx.locale
+    return { ...initialProps, locale: ctx?.locale || 'en' };
+  }
+
+  render = () => (
+    <Html dir={this.props.locale === 'fa' ? 'rtl' : 'ltr'} lang={this.props.locale}>
+      <Head>
+        <meta name="application-name" content="alibaba code challenge" />
+        <meta name="description" content="alibaba code challenge" />
+        {/*
+          <meta name="viewport" content="width=device-width, user-scalable=0" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default LocalizedDocument;
