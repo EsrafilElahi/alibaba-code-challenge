cc617327d0d07d2422645660a2313f934f0e9958
momhiar <momhiar@gmail.com>
2022-11-14 15:08:56 +0330

    add filter region search and handle loading

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = (props) => {
 
   return (
     <div>
-      <header className="w-full flex items-center justify-between shadow p-5 bg-lightModeBG dark:bg-darkModeBG">
+      <header className="w-full flex items-center justify-between shadow p-5 px-12 bg-lightModeBG dark:bg-darkModeBG">
         <div>
           <h1 className="font-nunito800 text-xl text-lightModeText dark:text-darkModeText">
             Where in the world?
