e637843e24ef94c82815fdf3b8ac22dfbf1fd5da
esrafilelahi <essinho1121@gmail.com>
2022-11-15 23:25:43 +0330

    make darkMode stable

--- components/ToggleMode.jsx
+++ components/ToggleMode.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import { BsMoon, BsSun } from "react-icons/bs";
 
 
 const ToggleMode = () => {
-  const [darkMode, setDarkMode] = useState(false)
 
-  const changeTheme = () => {
-    document.body.classList.toggle("dark")
+  const [darkMode, setDarkMode] = useState(undefined)
+
+  useEffect(() => {
+    setDarkMode(document.documentElement.classList.contains("dark"))
+  }, [])
+
+  useEffect(() => {
+    if (darkMode) {
+      window.document.documentElement.classList.add('dark')
+      localStorage.setItem("darkMode", true)
+    } else {
+      window.document.documentElement.classList.remove('dark')
+      localStorage.setItem("darkMode", false)
+    }
+  }, [darkMode])
+
+
+  const toggleTheme = () => {
     setDarkMode(!darkMode)
   }
 
+
   return (
-    <div className="flex cursor-pointer font-nunito600 text-lightModeText dark:text-darkModeText" onClick={changeTheme}>
+    <div className="flex cursor-pointer font-nunito600 text-lightModeText dark:text-darkModeText" onClick={toggleTheme}>
       {darkMode ? (
         <div className="flex items-center justify-center gap-2 hover:text-gray">
           <BsSun className="" />
